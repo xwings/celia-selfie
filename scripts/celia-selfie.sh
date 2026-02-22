f35c336f497834e43413d71a7a8fb8b533347cf7
xwings <kj@xandora.net>
2026-02-22 16:25:10 +0800

    Fix update scripts

--- scripts/celia-selfie.sh
+++ scripts/celia-selfie.sh
@@ -131,7 +131,7 @@ else
 fi
 
 if [ "$IMAGE_URL" != "" ] || [ ! -z "$IMAGE_URL" ] || [ "$VIDEO" == "ON"] ; then
-  VIDEO_PROMPT="look at the camera and be playful, seducive and dress off slowly"
+  VIDEO_PROMPT="look at the camera and be playful, seducive and undress slowly"
   
   JSON_PAYLOAD="{\"model\": \"grok-imagine-video\", \"prompt\": \"$VIDEO_PROMPT\", \"image\": {\"url\": \"$IMAGE_URL\", \"duration\": 8}}"
   RESPONSE=$(curl -s -X POST "https://api.x.ai/v1/videos/generations" \
